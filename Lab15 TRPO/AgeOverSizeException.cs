b65292a90299a7ec9f1077f27bde72a0699ba707
Yosinushi <magailya34@gmail.com>
2020-06-15 10:23:51 +0300

    comment

---
+++ Lab15 TRPO/AgeOverSizeException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab15_TRPO
+{
+    class AgeOverSizeException: ArgumentException
+    {
+        public int Value { get; }
+        public AgeOverSizeException(string message, int val)
+            : base(message)
+        {
+            Value = val;
+        }
+        public void Show()
+        {
+            Console.WriteLine(Message + $"ему {Value} лет");
+        }
+    }
+}
---
+++ Lab15 TRPO/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab15_TRPO
+{
+    class Person
+    {
+        public string Name;
+        private int age;
+        public Person(string name)
+        {
+            Name = name;
+        }
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value < 18)
+                { new AgeOverSizeException("Лицам до 18 регистрация запрещена", value).Show(); }
+                else
+                    age = value;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите кол-во персон");
+            int kolvo = Convert.ToInt32(Console.ReadLine());
+            Person[] people = new Person[kolvo];
+            for (int i = 0; i < kolvo; i++)
+            {
+                Console.WriteLine("Введите Имя");
+                string n = Console.ReadLine();
+                Console.WriteLine("Введите возраст");
+                int a = int.Parse(Console.ReadLine());
+                people[i] = new Person(n);
+                people[i].Age = a;
+                
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
